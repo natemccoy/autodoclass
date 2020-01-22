5644c846703d141c38bba9811232ce107c4b6626
Nathan McCoy <natemccoy@gmail.com>
2020-01-22 15:11:07 -0800

    tokenizer classes and related tests

--- autodoclass/tests/test_tokenizer.py
+++ autodoclass/tests/test_tokenizer.py
@@ -17,3 +17,53 @@ class TestTokenizer:
         """
         with pytest.raises(NotImplementedError):
             tokenizer.Tokenizer().transform(dummy_text)
+
+
+class TestWhitespaceTokenizer:
+    def test_init(self):
+        """ Test the initialization for the WhitespaceTokenizer class
+
+        :return None:
+        """
+        tokenizer.WhitespaceTokenizer()
+
+    def test_transform(self, dummy_text):
+        """ Test calling the transform function
+
+        :return None:
+        """
+        assert tokenizer.WhitespaceTokenizer().transform(dummy_text)
+
+    def test_transform_types(self, dummy_text):
+        """ Test the output types from the transform function
+
+        :return None:
+        """
+        tokens = tokenizer.WhitespaceTokenizer().transform(dummy_text)
+        assert isinstance(tokens, list)
+        assert isinstance(tokens.pop(), str)
+
+
+class TestSpacyTokenizer:
+    def test_init(self):
+        """ Test the initialization for the SpacyTokenizer class
+
+        :return None:
+        """
+        tokenizer.SpacyTokenizer()
+
+    def test_transform(self, dummy_text):
+        """ Test calling the transform function
+
+        :return None:
+        """
+        assert tokenizer.SpacyTokenizer().transform(dummy_text)
+
+    def test_transform_types(self, dummy_text):
+        """ Test the output types from the transform function
+
+        :return None:
+        """
+        tokens = tokenizer.WhitespaceTokenizer().transform(dummy_text)
+        assert isinstance(tokens, list)
+        assert isinstance(tokens.pop(), str)
--- autodoclass/tokenizer.py
+++ autodoclass/tokenizer.py
@@ -1,7 +1,43 @@
+import spacy
+
+
 class Tokenizer:
     def transform(self, text):
         """ Transform a given text input into tokens.
+        In tokenizer class, raise exception
 
-        :return list:
+        :param str text: input text to tokenize
+        :raises NotImplementedError:
         """
-        raise NotImplementedError("Transformation Not Implemented")
+        raise NotImplementedError("transform Not Implemented")
+
+
+class WhitespaceTokenizer(Tokenizer):
+    def transform(self, text):
+        """ Transform a given text input into tokens
+
+        :param str text: input text to tokenize
+        :return list(str): list of tokens (strings)
+        """
+        return text.split()
+
+
+class SpacyTokenizer(Tokenizer):
+    def __init__(self, language = 'en_core_web_sm'):
+        """ Initialize the Tokenizer
+
+        :param str language: spacy language model name
+        :return None:
+        """
+        self.nlp = spacy.load(
+            language,
+            disable = ['ner', 'parser', 'tagger', 'textcat']
+        )
+
+    def transform(self, text):
+        """ Transform a given text input into tokens
+
+        :param str text: input text to tokenize
+        :return list(str): list of tokens (strings)
+        """
+        return list(map(str, self.nlp(text)))
